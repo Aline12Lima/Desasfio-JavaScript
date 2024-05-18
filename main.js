fc97ef48825d1fcf74c9ccc445a80ccfebff821d
Aline <Aline.souza21@outlook.com.br>
2024-05-18 10:53:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+window.alert('Hello world!')
+
+
+var calc= window.prompt("Vamos somar dois números? ✍️");
+var firstNum= Number (window.prompt("Digite o primeiro número ☝️: "));
+var secondNum=Number (window.prompt("Digite o segundo número ✌️: "));
+
+var result= firstNum + secondNum
+
+window.alert(`O resultado da soma é ${result}. 🥳`);
+
+
+console.log(` o resultado é ${result}`)
+
+if (result = Number){
+    console.log(" O resultado não é string.")
+}else{
+        console.log(" O resultadao é uma string")
+    }

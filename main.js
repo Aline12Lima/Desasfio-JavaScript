985fe29f0b31b5d662faa585b57b73af28b583d7
Aline <Aline.souza21@outlook.com.br>
2024-05-20 12:32:42 -0300

    Respondendo o exercicio JS

--- main.js
+++ main.js
@@ -1,19 +1,79 @@
-window.alert('Hello world!')
+/*window.alert('Hello world!🌍');
 
+window.alert("Vamos somar dois números? ✍️");
 
-var calc= window.prompt("Vamos somar dois números? ✍️");
 var firstNum= Number (window.prompt("Digite o primeiro número ☝️: "));
 var secondNum=Number (window.prompt("Digite o segundo número ✌️: "));
 
 var result= firstNum + secondNum
 
-window.alert(`O resultado da soma é ${result}. 🥳`);
+window.alert( "O resultado é: ✍️ " + result);
 
 
-console.log(` o resultado é ${result}`)
 
-if (result = Number){
-    console.log(" O resultado não é string.")
+if(result ){
+
+    alert( result + " é um número");
 }else{
-        console.log(" O resultadao é uma string")
+    alert(  ' não é um número🛑');
+};
+
+
+
+var name = window.prompt("Qual o seu nome?");
+const resultName = name;
+
+if(resultName){
+    alert( resultName + " seu nome é uma String! 😁")
+}else{
+    alert("seu nome não é uma String.😿")
+}
+*/
+
+
+/*const isBoolean= true;
+const noBoolean= false;
+
+
+
+var resulBoolean = window.alert(isBoolean &&noBoolean);
+if(isBoolean &&noBoolean){
+    alert(" Resultado falso,isso é um boolean")
+}else{
+    alert("Resultado verdadeiro, isso é um boolean")
+}
+*/
+
+alert("Insira dois números para iniciar o cálculo : ")
+
+
+let firstNumber = prompt("Digite o primeiro número:")
+let secondNumber = prompt(" Digite o segundo número")
+
+firstNumber = Number (firstNumber)
+secondNumber = Number(secondNumber)
+
+
+const sub = firstNumber - secondNumber
+const div = firstNumber / secondNumber
+const multi = firstNumber * secondNumber
+
+
+
+alert("Subtração:" + sub + " =  " + parOuImpar(sub))
+alert("Multiplicação:" + multi + "= " + parOuImpar(multi))
+alert("Divisão:" + div + "= " + parOuImpar(parseInt(div, 10)))
+
+
+function parOuImpar(num) {
+    if (num % 2 === 0) {
+        return "é par";
+    } else {
+        return "é impar";
     }
+}
+
+
+
+
+
